e7d2cf97f63dabfd6cff73aa7be2e22d04419e6b
unknown <everest.k6@gmail.com>
2020-03-17 19:54:12 -0700

    initial-commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require('express')
+const userRouter = require('./users/userRouter')
 
-const server = express();
+const server = express()
+
+server.use(express.json())
+server.use("/users", userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,164 @@
 const express = require('express');
+const userHubs = require('./userDb')
+const postHubs = require('../posts/postDb')
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res, next) => {
+  userHubs.insert(req.body)
+      .then(users => {
+        res.status(201).json(users)
+      })
+      .catch(error => {
+        res.status(500).json({
+          message: 'Error retrieving the hubs',
+        });
+      });
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
-});
+router.post('/:id/posts', validateUser, async (req, res, next) => {
+  const newPosts = { ...req.body, post_id: req.params.id}
+  postHubs.insert(newPosts)
+          .then(posts => {
+            if(posts) {
+              res.status(201).json(posts)
+            } else {
+                res.status(404).json({
+                  message: "The post with the specified ID doen not exist.",
+                })
+            }
+          })
+          .catch(error => {
+            res.json(500).json({
+              error: "There was an error while saving the comment to the database", 
+            })
+          })
+})
 
 router.get('/', (req, res) => {
-  // do your magic!
+  userHubs.get(req.query)
+          .then(users => {
+            res.json(users);  
+          })
+          .catch(error => {
+            res.status(500).json({
+              message: 'Error retrieving the hubs',
+            });
+          });
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res, next) => {
+    res.status(200).json(req.user)
+  // userHubs.getById(req.params.id)
+  //         .then(user => {
+  //             if(user) {
+  //               res.json(user)
+  //             } else {
+  //               res.status(404).json({ 
+  //                 message: "The post with the specified ID does not exist." 
+  //               });
+  //             }
+  //         })
+  //         .catch(error => {
+  //             res.status(500).json({
+  //               message: 'Error retrieving the hubs',
+  //             });
+  //           });
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  userHubs.getUserPosts(req.params.id)
+          .then(posts => {
+            if(posts) {
+              res.json(posts)
+            } else {
+                res.status(404).json({ 
+                  message: "The post with the specified ID doen not exist.",
+                })
+            }
+          })
+          .catch(error => {
+            res.status(500).json({
+              message: 'Error retrieving the hubs',
+            });
+          });
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  userHubs.remove(req.params.id)
+          .then(user => {
+            if(user) {
+              return res.status(204).end()
+            } else {
+                res.status(404).json({ 
+                  message: "The post with the specified ID does not exist." 
+                })
+            }
+          })
+          .catch(error => {
+            res.status(500).json({
+              message: 'Error retrieving the hubs',
+            });
+          });
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
+  const changes = req.body;
+  userHubs.update(req.params.id, changes)
+          .then(users => {
+            if(users) {
+                res.json(users)
+            } else {
+                res.status(404).json({ 
+                  message: "The post with the specified ID doen not exist.",
+                })
+            }
+          })
+          .catch(error => {
+            res.status(500).json({
+              message: 'Error retrieving the hubs',
+            });
+          });
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  userHubs.getById(req.params.id)
+          .then(user => {
+              if(user) {
+                req.user = user
+
+                next()
+              } else {
+                res.status(404).json({ 
+                  message: "The post with the specified ID does not exist." 
+                });
+              }
+          })
+          .catch(error => {
+              res.status(500).json({
+                message: 'Error retrieving the hubs',
+              });
+            });
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body.name) {
+    return res.status(400).json({
+      message: "Please provide name.", 
+    })
+  }
+    next();
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if(!req.body.text) {
+    return res.status(400).json({
+      message: "Please provide text.", 
+    })
+  }
+    next();
 }
 
 module.exports = router;
